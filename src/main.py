a752f1f42c9ec3ce798cb4322feee15f5ba0690a
JF_Alferez <55857618+JuanfranZZ@users.noreply.github.com>
2025-05-07 20:07:21 +0200

    First commit

---
+++ src/main.py
@@ -0,0 +1,152 @@
+import streamlit as st
+import pandas as pd
+
+from utils import mean_location, distance_from_ref, get_pois
+
+from geographiclib.geodesic import Geodesic
+import osmnx as ox
+import folium
+from folium import plugins
+from streamlit_folium import st_folium
+from streamlit_folium import folium_static
+
+from tqdm import tqdm
+
+st.set_page_config(layout="wide")
+
+st.title("Meetpoint")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    # number of origin points
+    number = st.number_input(
+        "Number of points", value=0, placeholder="Type the number of participants...", step= 1, min_value=0)
+    st.write("The current number is ", number)
+    
+with col2:
+    selector = st.selectbox('Type of input', ['City', 'Coordinates'])
+
+st.subheader('Origin points')
+# data of each origin point
+orig_point = []
+
+for i in range(number):
+    orig_point.append(st.text_input(f"origin_{i+1} - Coordinates or Name of City", value=None, placeholder="Latitude, Longitude | Name of city"))
+    
+
+if all(orig_point) and number>0:
+    # select amenity
+    
+    st.subheader('Meeting point setup')
+    
+    chosen_category = st.selectbox('Choose your category', 
+                                   ('amenity', 'sport', 'leisure'))
+    if chosen_category == 'amenity':
+        list_tags = ['bar', 'restaurant', 
+                    'arts centre', 'bus station', 
+                    'casino', 'car rental', 'cinema', 
+                    'convention centre', 'events centre',
+                    'gym', 'hotel', 'juice bar', 'kiosk', 'library', 
+                    'park', 'planetarium', 'sauna', 'shop', 'spa', 'nightclub', 'pub']
+        
+    elif chosen_category == 'sport':
+        list_tags = ['climbing', 'soccer', 'billiards', 'darts', 'athletics', 'basketball',
+                     'beachvolleyball', 'billiards', 'bmx', 'bowls', 'boxing', 'canoe', 'climbing_adventure', 'crossfit',
+                     'cycling', 'dance', 'darts', 'fitness', 'golf', 'hiking', 'karting', 'kitesurfing', 'laser_tag',
+                     'miniature_golf', 'multi', 'paddle_tennis', 'padel', 'paintball', 'parachuting', 'parkour',
+                     'pelota', 'pickleball', 'pilates', 'racquet', 'roller_skating', 'running', 'scuba_diving',
+                     'shooting', 'skateboard', 'squash', 'surfing', 'swimming', 'table_tennis', 'table_soccer',
+                     'tennis', 'trampoline', 'ultimate', 'volleyball', 'water_polo', 'water_ski', 'windsurfing', 'yoga']
+        
+    elif chosen_category == 'leisure':
+        list_tags = ['sports_centre', 'sports_hall', 'stadium',
+                     'swimming_pool', 'recreation_ground',
+                     'golf_course', 'fitness_centre']
+        
+    chosen_tag = st.selectbox('Choose your tag', list_tags)
+    
+    # Definir las etiquetas para buscar
+    tags = {chosen_category: chosen_tag}
+    
+    # select distance
+    distance = st.number_input(label='Distance (m)', min_value=0, value='min', step=1)
+        
+    calculate = st.button("Calculate")
+    if calculate:
+        if selector == 'Coordinates':
+            Latitude = [float(lat.split(',')[0]) for lat in orig_point]
+            Longitude = [float(lon.split(',')[1]) for lon in orig_point]
+        elif selector == 'City':
+            Latitude = []
+            Longitude = []
+            for point in orig_point:
+                Latitude.append(ox.geocode_to_gdf(point).centroid.get_coordinates()['y'].values[0])
+                Longitude.append(ox.geocode_to_gdf(point).centroid.get_coordinates()['x'].values[0])
+
+        coordinates = {}
+        
+        for i in range(number):
+            coordinates[f"origin_{i+1}"] = {"Latitude": Latitude[i], "Longitude": Longitude[i], "colour": "#0044ff"}
+
+
+        # meetpoint
+        #mp1 = meetpoint([(l[0], l[1]) for l in list(zip(Latitude, Longitude))])
+        #coordinates['meetpoint1'] = {"Latitude": mp1[0], "Longitude": mp1[1], "colour":"#ff0033"}
+        
+        mp = mean_location(pd.DataFrame(coordinates).transpose())
+        coordinates['meetpoint'] = {"Latitude": mp[0], "Longitude": mp[1], "colour":"#1ff035"}
+        
+        # table showing data
+        df = pd.DataFrame(coordinates).transpose()
+        st.table(df[['Latitude','Longitude']])
+        
+        # mapa para puntos de coordenadas
+        map1 = st.map(df, latitude='Latitude', longitude='Longitude', size=20, color='colour')
+        
+        print('Meetpoint spherical:')
+        for k,v in coordinates.items():
+            distance_from_ref(k, (v['Latitude'], v['Longitude']), (coordinates['meetpoint']['Latitude'], coordinates['meetpoint']['Longitude']))
+        
+        
+        # crear segundo mapa para los puntos de interés
+        st.subheader('Map with points of interest')
+   
+        m = folium.Map(location=[coordinates['meetpoint']['Latitude'], coordinates['meetpoint']['Longitude']], zoom_start=15)
+        
+        folium.Circle(location=[coordinates['meetpoint']['Latitude'], coordinates['meetpoint']['Longitude']],
+                      radius=distance, opacity=0.6, fill=True).add_to(m)
+        
+        pois = get_pois((coordinates['meetpoint']['Latitude'], coordinates['meetpoint']['Longitude']), tags=tags, distance=distance)
+
+        if pois is None:
+            # pois is None when not found
+            st.warning(f'{chosen_tag} not found closer than {distance} from meetpoint!')
+        else:
+            for i, p in tqdm(pois.iterrows()):
+                folium.Marker(location=[float(p['Latitude']),float(p['Longitude'])], 
+                            tooltip=folium.Tooltip(p['name'], permanent=True)).add_to(m)
+            
+            for k, v in coordinates.items():
+                if 'meet' not in k:
+                    color='green'
+                    icon_='bookmark'
+                else:
+                    color='purple'
+                    icon_='flag'
+                    
+                folium.Marker(location=[float(v['Latitude']), float(v['Longitude'])],
+                            tooltip=folium.Tooltip(k, permanent=True), 
+                            icon=folium.Icon(icon=icon_, color=color)).add_to(m)
+                
+            map_a = folium_static(m, width=800, height=800)
+        
+        
+        
+        
+        
+        
+    
+
+
+
---
+++ src/utils.py
@@ -0,0 +1,192 @@
+
+import numpy as np
+import math
+import osmnx as ox
+
+"""
+def midpoint(point1, point2):
+    return np.array([np.mean([point1[0], point2[0]]), np.mean([point1[1], point2[1]])])
+
+def perpendicular(vector):
+    vector_t = np.array([-vector[1], vector[0]]) / np.linalg.norm(vector)
+    return vector_t
+
+def vector(p1, p2):
+    return (p2-p1) / np.linalg.norm(p2-p1)
+
+def three_points_intersection(p1, p2, p3):
+    "Three points can define two mediatrix which intersection is the searched point.
+
+    Args:
+        p1 (_type_): _description_
+        p2 (_type_): _description_
+        p3 (_type_): _description_
+    "
+    mid_points = [midpoint(p1,p2), midpoint(p2,p3)]
+    vectors = [vector(p1,p2), vector(p2,p3)]
+    mediatrix = [perpendicular(v) for v in vectors]
+    aux_points = [mid_points[0] + mediatrix[0], mid_points[1] + mediatrix[1]]
+    
+    mid_points.extend(aux_points)
+    points = mid_points
+    
+    # Cramer (intersection between line p1-p3 and line p2-p4)
+    x1 = points[0][0]; y1 = points[0][1]
+    x2 = points[2][0]; y2 = points[2][1]
+    x3 = points[1][0]; y3 = points[1][1]
+    x4 = points[3][0]; y4 = points[3][1]
+
+    m1 = np.array([[x1*y2-x2*y1, x1-x2],[x3*y4-x4*y3, x3-x4]])
+    m2 = np.array([[y2-y1, x1-x2],[y4-y3, x3-x4]])
+    m3 = np.array([[y2-y1, x1*y2-x2*y1],[y4-y3, x3*y4-x4*y3]])
+    m4 = np.array([[y2-y1, x1-x2],[y4-y3, x3-x4]])
+    
+    x_inter = np.linalg.det(m1)/np.linalg.det(m2)
+    y_inter = np.linalg.det(m3)/np.linalg.det(m4)
+    
+    return (x_inter, y_inter)
+
+
+def meetpoint(*args, dist=2000, fairness=1.0):
+    "Function to obtain the midpoint or mid points
+
+    Args:
+        *points: earch arg should be a point (tuple point)
+        dist (int, optional): distance from point to search pois. Defaults to 2000.
+        fairness (float, optional): unbalanced search. Defaults to 1.0.
+    "
+    in_points = args[0]
+    points = []
+    for p in in_points:
+        points.append(np.array(p))
+    
+    N = len(points)
+    
+    if N < 2:
+        print('N<2')
+        result = points[0]
+    
+    elif N == 2:
+        print('N==2')
+        result = midpoint(points[0], points[1])
+    
+    elif N == 3:
+        print('N==3')
+        result = three_points_intersection(points[0], points[1], points[2])
+    
+    else:
+        print(f'N={N}')
+        new_points = points_reduction_to_three(points)
+        result = meetpoint(new_points, dist=dist, fairness=fairness)
+        
+    return result
+        
+        
+
+        
+        
+def points_reduction_to_three(*args):
+    in_points = args[0]
+    N = len(in_points)
+    if N > 3:
+        points = {}
+        distance = {}
+        for i, arg in enumerate(in_points):
+            points[f'{i}'] = arg
+        for i in range(N):
+            for j in range(i+1, N):
+                distance[f"{i}-{j}"] = np.linalg.norm(points[f'{i}'] - points[f'{j}'])
+        min_val = np.inf
+        for key, val in distance.items():
+            if val < min_val:
+                min_val = val
+                min_key = key
+        # as i < j always when we detect the min distance, the new point will replace the i value and the j will be deleted to create new dict
+        i_point = min_key.split('-')[0]
+        j_point = min_key.split('-')[1]
+        points[f'{i_point}'] = midpoint(points[f'{i_point}'],points[f'{j_point}'])
+        del points[f'{j_point}']
+        
+        result = points_reduction_to_three(np.array(list(points.values())))    
+    
+    else:
+        result = in_points.copy()
+        
+    return result
+
+"""
+
+    
+def distance_from_ref(name, point, ref_point):
+    print(f'dist {name}: {distance(point, ref_point)}')
+    
+def distance(p1, p2):
+    p1_ = np.array(p1)
+    p2_ = np.array(p2)
+    return np.linalg.norm(p2_ - p1_)
+
+def fairness(*args):
+    
+    total = np.sum(args)
+    N = len(args)
+    
+    dist = []
+    
+    for arg in args:
+        dist.append(arg/total*N)
+        
+    return dist      
+
+def mean_location(coords_df):
+    x = 0.0
+    y = 0.0
+    z = 0.0
+
+    for i, coord in coords_df.iterrows():
+        latitude = math.radians(coord['Latitude'])
+        longitude = math.radians(coord['Longitude'])
+
+        x += math.cos(latitude) * math.cos(longitude)
+        y += math.cos(latitude) * math.sin(longitude)
+        z += math.sin(latitude)
+
+    total = len(coords_df)
+
+    x = x / total
+    y = y / total
+    z = z / total
+
+    central_longitude = math.atan2(y, x)
+    central_square_root = math.sqrt(x * x + y * y)
+    central_latitude = math.atan2(z, central_square_root)
+
+    mean_location_ = {
+        'latitude': math.degrees(central_latitude),
+        'longitude': math.degrees(central_longitude)
+        }
+        
+    return (mean_location_['latitude'], mean_location_['longitude'])
+
+def get_pois(center_point, tags, distance=2000):
+    try:
+        gdf_pois = ox.features_from_point(center_point, tags=tags, dist=distance)[['geometry','name']]
+        pois = gdf_pois[gdf_pois['name'].notna()].loc[('node',)]
+        coords = pois.get_coordinates(ignore_index=True)
+        names = pois['name'].values
+        coords['name'] = names
+        
+        result = coords.rename(columns={'x':'Longitude', 'y': 'Latitude'})
+        
+        return result
+    
+    except Exception:#KeyError or osmnx._errors.InsufficientResponseError:
+        
+        print(f'{tags} not found!')
+        return None
+
+if __name__=='__main__':
+    
+    # testing meetpoint
+
+    # mp = meetpoint([(0,1),(1,0),(0,0),(-1,0),(2,0), (2,2)])
+    pass
